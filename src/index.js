503de6e2bb42746fff4f6dd20c44df8d5728a5bc
Tom Mulkins <tom.mulkins@gmail.com>
2019-12-01 13:56:02 -0500

    Use prettier with standard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const http = require('http')
 
-http.createServer((req, res) => {
-  res.write('Hello World!')
-  res.end()
-}).listen(8080)
+http
+  .createServer((req, res) => {
+    res.write('Hello World!')
+    res.end()
+  })
+  .listen(8080)
 
 console.log('Server started!  Listening on port 8080')
