a69323a79e1a295a997a44edf043acd33c810fa1
Tom Mulkins <tom.mulkins@gmail.com>
2019-12-08 18:59:07 -0500

    set header and status code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.set({
+    'Content-Type': 'text/html',
+    'X-tom-says': 'Hi!'
+  })
+  res.status(418).send('Hello World!')
 })
 
 app.listen(port, _ => console.log(`Example app listening on port ${port}!`))
